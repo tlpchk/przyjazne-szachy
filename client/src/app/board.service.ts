b2b5eece63b3e8aa3b334ea06af9f92bc968935c
tlpchk <max.telepchuk@gmail.com>
2018-11-14 01:18:43 +0100

    Board service with http request

--- client/src/app/board.service.ts
+++ client/src/app/board.service.ts
@@ -1,8 +1,13 @@
-import {Injectable, OnInit} from '@angular/core';
-import {BOARD} from './mock-board';
-import {Observable, of} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {forkJoin, Observable, of} from 'rxjs';
 import {Cell} from './cell';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {MessageService} from "./message.service";
+import {catchError, map, tap} from 'rxjs/operators';
 
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
@@ -10,12 +15,63 @@ import {Cell} from './cell';
 export class BoardService{
 
   board: Cell[];
+  private boardUrl = 'api/board';
+
+  constructor(private http: HttpClient,
+              private messageService: MessageService){}
 
   getBoard(): Observable<Cell[]> {
-    return of(BOARD);
+    return this.http.get<Cell[]>(this.boardUrl)
+        .pipe(
+        tap(_ => this.log('fetched board')),
+        catchError(this.handleError('getBoard', []))
+    );
   }
 
-    makeMove(srcCell: Cell, dscCell: Cell): void {
-      console.log('User want to make move from %d to %d', srcCell.id , dscCell.id);
-    }
+  private log(message: string) {
+      this.messageService.add(`BoardService: ${message}`);
+  }
+
+
+  updateCell(cell: Cell): Observable<any>{
+      return this.http.put(this.boardUrl,
+          cell
+          , httpOptions).pipe(
+             tap(_ => this.log(`udated cell=${cell.id}`)),
+            catchError(this.handleError<any>('updateCell'))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+
+          // TODO: send the error to remote logging infrastructure
+          console.error(error); // log to console instead
+
+          // TODO: better job of transforming error for user consumption
+          this.log(`${operation} failed: ${error.message}`);
+
+          // Let the app keep running by returning an empty result.
+          return of(result as T);
+      };
+  }
+
+    /*TODO: Don't pipe logs*/
+    /*makeMove(moveElement: Cell, moveElement2: Cell) {
+        forkJoin(
+            this.updateCell(moveElement),
+            this.updateCell(moveElement2)
+        ).pipe(
+            map(([first, second]) => {
+                // combineLatest returns an array of values, here we map those values to an object
+                return { first, second };
+            })
+        );
+    }*/
 }
