af15bff730a224d6d51b11f3ecfd35909b0d9979
tlpchk <max.telepchuk@gmail.com>
2018-11-14 01:16:03 +0100

    Manage routs

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BoardComponent} from './board/board.component';
+
+const routes: Routes = [
+    { path: 'board', component: BoardComponent },
+    {path: '', redirectTo: '/board', pathMatch: 'full'}
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
