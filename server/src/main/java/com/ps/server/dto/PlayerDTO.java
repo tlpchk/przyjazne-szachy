37d1d6dd630b814e833e1ac11e88a82d011c9b8a
sawicki-rafal <sawickirafal@outlook.com>
2018-11-30 04:05:42 +0100

    merging

---
+++ server/src/main/java/com/ps/server/dto/PlayerDTO.java
@@ -0,0 +1,14 @@
+package com.ps.server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlayerDTO {
+    private Long id;
+}
