1027fbb668f511f827a71d03ab1e2db6bc7d7b01
tlpchk <max.telepchuk@gmail.com>
2018-12-13 14:52:11 +0100

    Styling sing in and sing up pages.

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {AuthService} from "../../_services/auth.service";
 import {Router} from "@angular/router";
 
 @Component({
+    encapsulation: ViewEncapsulation.None,
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['../auth.component.scss']
 })
 export class LoginComponent implements OnInit {
 
    username = "";
    password = "";
 
-  constructor(private authService: AuthService,
+  constructor(private auth: AuthService,
               private router: Router) { }
 
   ngOnInit() {
   }
 
   loginUser(){
-      this.authService.getUserDetails(this.username,this.password).subscribe(data=> {
+      this.auth.logInUser(this.username,this.password).subscribe(data=> {
         console.log(this.username,this.password);
           if (data.success) {
-              this.authService.setLoggedIn(true);
+              this.auth.setLoggedIn(true);
               this.router.navigate(['/user/home']);
           }else{
             window.alert(data.message)
--- client/src/app/auth/regist/regist.component.ts
+++ client/src/app/auth/regist/regist.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../_services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-regist',
   templateUrl: './regist.component.html',
-  styleUrls: ['./regist.component.scss']
+  styleUrls: ['../auth.component.scss']
 })
 export class RegistComponent implements OnInit {
+  username: string;
+  email: string;
+  password1: string;
+  password2: string;
 
-  constructor() { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
+  registerUser() {
+    this.auth.registerUser(this.username,this.email,this.password1,this.password2).subscribe( data =>{
+      if (data.success) {
+        this.auth.setLoggedIn(true);
+        this.router.navigate(['/user/home']);
+      }else{
+        window.alert(data.message)
+      }
+    })
+  }
+
+
 }
+
