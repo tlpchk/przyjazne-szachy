14f24b5db3711b930d4cf65cc064397c2f5ad0be
sawicki-rafal <sawickirafal@outlook.com>
2019-01-23 21:52:14 +0100

    fixed urls

--- client/src/app/_services/httpConection.ts
+++ client/src/app/_services/httpConection.ts
@@ -1,25 +1,25 @@
 import {HttpHeaders} from "@angular/common/http";
 
 /** @ignore*/
-export const loginUrl = 'https://przyjazne-szachy.herokuapp.com/api/login';
+export const loginUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/login';
 /** @ignore*/
-export const registerUrl = 'https://przyjazne-szachy.herokuapp.com/api/register';
+export const registerUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/register';
 /** @ignore*/
-export const  logoutUrl = 'https://przyjazne-szachy.herokuapp.com/api/bye';
+export const  logoutUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/bye';
 /** @ignore*/
-export const  profileUrl = 'https://przyjazne-szachy.herokuapp.com/api/user';
+export const  profileUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/user';
 /** @ignore*/
-export const  gamesUrl = 'https://przyjazne-szachy.herokuapp.com/api/games';
+export const  gamesUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/games';
 /** @ignore*/
-export const playersUrl = 'https://przyjazne-szachy.herokuapp.com/api/players';
+export const playersUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/players';
 /** @ignore*/
-export const verifyUrl = 'https://przyjazne-szachy.herokuapp.com/api/verify';
+export const verifyUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/verify';
 /** @ignore*/
-export const resetUrl = 'https://przyjazne-szachy.herokuapp.com/api/reset';
+export const resetUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/reset';
 /** @ignore*/
-export const changeUrl = 'https://przyjazne-szachy.herokuapp.com/api/reset/change';
+export const changeUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/reset/change';
 /** @ignore*/
-export const rankingUrl = 'https://przyjazne-szachy.herokuapp.com/api/ranking';
+export const rankingUrl = 'https://przyjazne-szachy.herokuapp.com:8080/api/ranking';
 /** @ignore*/
 export const  moveSubUrl = '/move';
 /** @ignore*/
